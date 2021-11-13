95b1d2006c1ac168cdb5988aaa8512ee43f699d3
user-github <schneidergw@yahoo.com>
2021-11-13 18:58:59 +0700

    vue,tailwind,router boilerplate

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+      <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /srcs
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+
