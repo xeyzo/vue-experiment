24d1d181306c52e04ca223f11e0d5678e7abbb7c
xeyzo <schneidergw@yahoo.com>
2021-11-16 23:05:22 +0700

    repair navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,18 +4,14 @@
         <router-link to="/">Rumah-Sandi</router-link>
     </div>
     <div class="w-1/2 flex bg-red-100 py-5">
-        <div class="w-1/8 flex">
-            <div class="mr-3 w-1/2">
-                <router-link to="/" class="w-1/2 hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">Home</router-link>
-            </div>
-            <div class="mr-3 w-1/8">
-                <router-link to="/about" class="hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">About</router-link>
-            </div>
+        <div class="mr-3 w-1/8">
+            <router-link to="/" class="w-1/2 hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">Home</router-link>
         </div>
-        <div class="w-1/2">
-         <div class="mr-3 w-1/8 text-end">
+        <div class="mr-3 w-1/8">
+            <router-link to="/about" class="hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">About</router-link>
+        </div>
+         <div class="mr-3 w-1/8 bg-text-right">
             <h1>ini</h1>
-        </div>
         </div>
         <router-view/>
     </div>
