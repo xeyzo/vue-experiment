d355ca18e6719999c17aeb10166e48f2dab3e51b
sandiagung <sandi.agung@ecampus.ut.ac.id>
2021-11-17 11:12:47 +0700

    ready navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,37 @@
 <template>
+<div class="container mx-auto">
   <div class="navbar flex mb-4 w-auto text-left h-16">
-    <div class="w-1/2 px-2 bg-red-200 py-5">
-        <router-link to="/">Rumah-Sandi</router-link>
+    <div class="w-1/2 px-2 py-2">
+        <router-link to="/">
+        <img src = "../assets/Logo-UT_03.png">
+        </router-link>
     </div>
-    <div class="w-1/2 flex bg-red-100 py-5">
-        <div class="mr-3 w-1/8">
-            <router-link to="/" class="w-1/2 hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">Home</router-link>
+    <div class="w-1/2 flex py-5">
+        <div class="mr-5 w-1/8">
+            <router-link to="/" class="text-blue-500 py-1 px-3">Home</router-link>
         </div>
-        <div class="mr-3 w-1/8">
-            <router-link to="/about" class="hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3">About</router-link>
+        <div class="mr-5 w-1/8">
+            <router-link to="/about" class="text-blue-500 py-1 px-3">About UT</router-link>
         </div>
-        <div class="mr-3 w-1/8">
-            <div class="">Profil</div>
+        <div class="mr-5 w-1/8 text-blue-500">
+            <div class="">Akademik</div>
         </div>
-         <div class="w-6/8 text-end">
-            <div class="">ini</div>
+        <div class="mr-5 w-1/8 text-blue-500">
+            <div class="">UT Online</div>
+        </div>
+        <div class="mr-5 w-1/8 text-blue-500">
+            <div class="">Fasilitas</div>
+        </div>
+        <div class="mr-5 w-1/8 text-blue-500">
+            <div class="">Berita</div>
+        </div>
+        <div class=" w-1/8 text-blue-500">
+            <div class="">Pengumuman</div>
         </div>
         <router-view/>
     </div>
   </div>
+</div>
 </template>
 
 
